4efee0548018aea4ce2e04c83b6555ec1f4cb246
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2025-12-25 00:06:16 +0300

    API Status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,42 @@
+/*
+ * Must use jQuery
+ * If the checkbox is checked, store the Amenity ID in a variable (dictionary or list)
+ * If unchecked, remove the Amenity ID from the variable
+ * If in the status is “OK”, add the class available to the div#api_status
+ * Otherwise, remove the class available to the div#api_status
+ */
+$(function () {
+  const checkBox = $('input[type="checkbox"]').css('margin-right', '10px');
+
+  let amenitiesList = [];
+
+  $(checkBox).on('change', function () {
+    const name = $(this).data('name');
+
+    if (this.checked) {
+      amenitiesList.push(name);
+      if (amenitiesList.length) {
+        $('.amenities h4').text(`${amenitiesList.join(', ')}`);
+      } else {
+        $('.amenities h4').text('Amenities');
+      }
+    } else {
+      amenitiesList = amenitiesList.filter(item => item !== name);
+    }
+  });
+
+  $.ajax({
+    url: 'http://localhost:5001/api/v1/status/',
+    method: 'GET',
+    dataType: 'json'
+  })
+  .done(function () {
+    $('#api_status').addClass('available');
+  })
+  .fail(function () {
+    $('#api_status').removeClass('available');
+  })
+  .always(function () {
+    console.log("API check is complete!");
+  })
+});
